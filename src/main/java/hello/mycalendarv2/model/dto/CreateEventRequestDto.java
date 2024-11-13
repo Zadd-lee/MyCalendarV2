b6056a09f984998ded528b42d0a5aea8e81cf1fe
이자드 <kimminkyeong0418@gmail.com>
2024-11-13 21:36:43 +0900

    feat: bean Validation 기능 추가

--- src/main/java/hello/mycalendarv2/model/dto/CreateEventRequestDto.java
+++ src/main/java/hello/mycalendarv2/model/dto/CreateEventRequestDto.java
@@ -1,6 +1,7 @@
 package hello.mycalendarv2.model.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ public class CreateEventRequestDto {
     private String title;
     private String contexts;
     @NotNull
+    @Size(max = 8)
     private String userName;
 
 }
