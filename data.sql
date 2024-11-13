83f9e6e74564f59b3f061e59dfc1693836251381
이자드 <kimminkyeong0418@gmail.com>
2024-11-13 17:05:17 +0900

    doc:column 이름 변경

--- data.sql
+++ data.sql
@@ -1,14 +1,14 @@
-insert into user (NAME, EMAIL, PASSWORD, CREATEDAT, UPDATEDAT)
+insert into user (NAME, EMAIL, PASSWORD, CREATED_AT, UPDATED_AT)
 values ('user1', 'user1@gmail.com', '1111', now(), now());
-insert into user (NAME, EMAIL, PASSWORD, CREATEDAT, UPDATEDAT)
+insert into user (NAME, EMAIL, PASSWORD, CREATED_AT, UPDATED_AT)
 values ('user1', 'user@gmail.com', '1111', now(), now());
 
 
-insert into event (USERID, TITLE, CONTEXTS, CREATEDAT, UPDATEDAT)
+insert into event (USERID, TITLE, CONTEXTS, CREATED_AT, UPDATED_AT)
 values (1,'test1','test1',now(),now());
-insert into event (USERID, TITLE, CONTEXTS, CREATEDAT, UPDATEDAT)
+insert into event (USERID, TITLE, CONTEXTS, CREATED_AT, UPDATED_AT)
 values (1,'test2','test2',now(),now());
-insert into event (USERID, TITLE, CONTEXTS, CREATEDAT, UPDATEDAT)
+insert into event (USERID, TITLE, CONTEXTS, CREATED_AT, UPDATED_AT)
 values (2,'test22',null,now(),now());
 
 
