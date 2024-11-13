7a0237d5d6137a9df63dfcb487676a3046daf9a0
이자드 <kimminkyeong0418@gmail.com>
2024-11-13 18:42:11 +0900

    doc : sql 위치 변경 및 사용 설정 완료

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
 insert into user (NAME, EMAIL, PASSWORD, CREATED_AT, UPDATED_AT)
 values ('user1', 'user1@gmail.com', '1111', now(), now());
 insert into user (NAME, EMAIL, PASSWORD, CREATED_AT, UPDATED_AT)
-values ('user1', 'user@gmail.com', '1111', now(), now());
+values ('user2', 'user@gmail.com', '1111', now(), now());
 
 
-insert into event (USERID, TITLE, CONTEXTS, CREATED_AT, UPDATED_AT)
+insert into event (USER_ID, TITLE, CONTEXTS, CREATED_AT, UPDATED_AT)
 values (1,'test1','test1',now(),now());
-insert into event (USERID, TITLE, CONTEXTS, CREATED_AT, UPDATED_AT)
+insert into event (USER_ID, TITLE, CONTEXTS, CREATED_AT, UPDATED_AT)
 values (1,'test2','test2',now(),now());
-insert into event (USERID, TITLE, CONTEXTS, CREATED_AT, UPDATED_AT)
+insert into event (USER_ID, TITLE, CONTEXTS, CREATED_AT, UPDATED_AT)
 values (2,'test22',null,now(),now());
 
 
