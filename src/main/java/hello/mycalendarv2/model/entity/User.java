6c630ac2c03e053a7733de008ee463320e6db65f
이자드 <kimminkyeong0418@gmail.com>
2024-11-13 15:05:12 +0900

    feat: 사용자 객체 추가

--- src/main/java/hello/mycalendarv2/model/entity/User.java
+++ src/main/java/hello/mycalendarv2/model/entity/User.java
@@ -1,4 +1,32 @@
 package hello.mycalendarv2.model.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name="user")
+@NoArgsConstructor
+public class User extends DateEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Event> events = new ArrayList<>();
 }
