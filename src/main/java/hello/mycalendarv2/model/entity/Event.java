0f8fe82e77c3cdf0f47a52e2f07ae6c8ef6dc974
이자드 <kimminkyeong0418@gmail.com>
2024-11-13 15:05:00 +0900

    feat: 일정 객체 추가

--- src/main/java/hello/mycalendarv2/model/entity/Event.java
+++ src/main/java/hello/mycalendarv2/model/entity/Event.java
@@ -1,4 +1,21 @@
 package hello.mycalendarv2.model.entity;
 
-public class Event {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "event")
+public class Event extends DateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "EVENT_ID")
+    Long id;
+
+    String title;
+
+    String contexts;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
 }
