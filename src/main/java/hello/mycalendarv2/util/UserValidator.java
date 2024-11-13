b61325c044bef3d3cf7805902ea5ac1c986181d0
이자드 <kimminkyeong0418@gmail.com>
2024-11-13 18:54:18 +0900

    refact: 유저 검증 메서드 변경

--- src/main/java/hello/mycalendarv2/util/UserValidator.java
+++ src/main/java/hello/mycalendarv2/util/UserValidator.java
@@ -1,13 +1,12 @@
 package hello.mycalendarv2.util;
 
-import hello.mycalendarv2.model.entity.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 public class UserValidator {
-    public void validatePassword(User user, String password) {
+    public void validatePassword(String password, String inputPassword) {
 
-        if (!user.getPassword().equals(password)) {
+        if (!password.equals(inputPassword)) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다");
         }
     }
